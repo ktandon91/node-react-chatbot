59417c70c82ba59f3545b5c3e829e7b3b41e9846
Karan <karantandon1991@gmail.com>
2020-08-12 16:29:59 +0530

    json parse to try fix key issue

--- config/prod.js
+++ config/prod.js
@@ -3,5 +3,5 @@ module.exports = {
     dialogFlowSessionID:process.env.DIALOGFLOW_SESSION_ID,
     mongoURI:process.env.MONGO_URI,
     googleClientEmail:process.env.GOOGLE_CLIENT_EMAIL,
-    googlePrivateKey:process.env.GOOGLE_PRIVATE_KEY
+    googlePrivateKey:JSON.parse(process.env.GOOGLE_PRIVATE_KEY)
 }

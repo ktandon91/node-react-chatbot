2721aeea6e6c50a4d083f46b1569324267f56cc9
Karan <karantandon1991@gmail.com>
2020-08-12 19:57:44 +0530

    deployment heroku 4

--- index.js
+++ index.js
@@ -13,6 +13,15 @@ app.use(bodyParser.json());
 
 require('./routes/dialogFlowRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+    // js and css files
+    app.use(express.static('client/build'));
+    // index.html for all page routes
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(_dirname,'client','build','index.html'));
+    });
+}
 
 const PORT = process.env.PORT||5000;
 app.listen(PORT);
--- routes/dialogFlowRoutes.js
+++ routes/dialogFlowRoutes.js
@@ -2,9 +2,9 @@ const chatbot = require('../chatbot/chatbot')
 
 module.exports = app => {
 
-app.get('/', (req, res) => {
-    res.send({'hello': "World!"})
-});
+// app.get('/', (req, res) => {
+//     res.send({'hello': "World!"})
+// });
 
 app.post('/api/df_text_query', async (req, res) => { 
   // The text query request.
